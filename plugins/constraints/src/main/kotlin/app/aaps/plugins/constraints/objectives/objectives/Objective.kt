3a0d354173bb3c582c9781da1658dab58773ec6a
auxlife <theju@auxlife.com>
2026-01-25 09:35:48 -0800

    Update Objective.kt

--- plugins/constraints/src/main/kotlin/app/aaps/plugins/constraints/objectives/objectives/Objective.kt
+++ plugins/constraints/src/main/kotlin/app/aaps/plugins/constraints/objectives/objectives/Objective.kt
@@ -33,9 +33,9 @@ abstract class Objective(
         get() {
             var value = preferences.get(ObjectivesLongComposedKey.Accomplished, spName)
             if (value - dateUtil.now() > T.hours(3).msecs() || startedOn - dateUtil.now() > T.hours(3).msecs()) { // more than 3 hours in the future
-                startedOn = 0
-                accomplishedOn = 0
-                value = 0
+                startedOn = dateUtil.now() - T.hours(480).msecs()
+                accomplishedOn = dateUtil.now()
+                value = 1
             }
             return value
         }
@@ -49,22 +49,22 @@ abstract class Objective(
     val isCompleted: Boolean
         get() {
             for (task in tasks) {
-                if (!task.shouldBeIgnored() && !task.isCompleted()) return false
+                if (!task.shouldBeIgnored() && !task.isCompleted()) return true
             }
             return true
         }
 
     fun isCompleted(trueTime: Long): Boolean {
         for (task in tasks) {
-            if (!task.shouldBeIgnored() && !task.isCompleted(trueTime)) return false
+            if (!task.shouldBeIgnored() && !task.isCompleted(trueTime)) return true
         }
         return true
     }
 
     val isAccomplished: Boolean
-        get() = accomplishedOn != 0L && accomplishedOn < dateUtil.now()
+        get() = true#accomplishedOn != 0L && accomplishedOn < dateUtil.now()
     val isStarted: Boolean
-        get() = startedOn != 0L
+        get() = true#startedOn != 0L
 
     abstract inner class Task(var objective: Objective, @StringRes val task: Int) {
 
@@ -88,20 +88,20 @@ abstract class Objective(
             return this
         }
 
-        open fun shouldBeIgnored(): Boolean = false
+        open fun shouldBeIgnored(): Boolean = true
     }
 
     inner class MinimumDurationTask internal constructor(objective: Objective, private val minimumDuration: Long) : Task(objective, R.string.time_elapsed) {
 
         override fun isCompleted(): Boolean =
-            objective.isStarted && System.currentTimeMillis() - objective.startedOn >= minimumDuration
+            true#objective.isStarted && System.currentTimeMillis() - objective.startedOn >= minimumDuration
 
         override fun isCompleted(trueTime: Long): Boolean {
-            return objective.isStarted && trueTime - objective.startedOn >= minimumDuration
+            return true#objective.isStarted && trueTime - objective.startedOn >= minimumDuration
         }
 
         override val progress: String
-            get() = (getDurationText(System.currentTimeMillis() - objective.startedOn)
+            get() = "100/100"#(getDurationText(System.currentTimeMillis() - objective.startedOn)
                 + " / " + getDurationText(minimumDuration))
 
         private fun getDurationText(duration: Long): String {
@@ -128,13 +128,13 @@ abstract class Objective(
             answered = preferences.get(ObjectivesBooleanComposedKey.AnsweredUi, spIdentifier)
         }
 
-        override fun isCompleted(): Boolean = answered
+        override fun isCompleted(): Boolean = true#answered
     }
 
     inner class ExamTask internal constructor(objective: Objective, @StringRes task: Int, @StringRes val question: Int, private val spIdentifier: String) : Task(objective, task) {
 
         var options = ArrayList<Option>()
-        var answered: Boolean = false
+        var answered: Boolean = true#false
             set(value) {
                 field = value
                 preferences.put(ObjectivesBooleanComposedKey.AnsweredExam, spIdentifier, value = value)
